1bd49ca774587cd874718f3399fccc533c77c047
Jon Deibel <Jonathan_Deibel@intuit.com>
2015-04-06 21:31:34 -0700

    Using proper location

--- lib/kitchenplan/config.rb
+++ lib/kitchenplan/config.rb
@@ -126,13 +126,13 @@ module Kitchenplan
       unless src.nil?
         # Create a user in knife. For some reason it didn't want to accept one that i made externally
         puts("Running Command: sudo knife user create devadmin -f /Users/#{ENV['USER']}/.chef/#{ENV['USER']}.pem -a -p password -z")
-        system("sudo knife user delete #{ENV['USER']}")
-        system("sudo knife user create #{ENV['USER']} -f /Users/#{ENV['USER']}/.chef/#{ENV['USER']}.pem -a -p password -z")     
+        system("sudo vendor/bin/knife user delete #{ENV['USER']}")
+        system("sudo vendor/bin/knife user create #{ENV['USER']} -f /Users/#{ENV['USER']}/.chef/#{ENV['USER']}.pem -a -p password -z")     
 
         # Actaully create the vault that is used :D
         puts("Running Command: knife vault create secret_vault secret_attributes '#{src.to_json}' -z")
-        system("knife vault delete secret_vault secret_attributes -z")
-        system("knife vault create secret_vault secret_attributes '#{src.to_json}' -z -A #{ENV['USER']}")
+        system("knife vendor/bin/vault delete secret_vault secret_attributes -z")
+        system("knife vendor/bin/vault create secret_vault secret_attributes '#{src.to_json}' -z -A #{ENV['USER']}")
         puts("Test")
 
         src.each do |key, array|
