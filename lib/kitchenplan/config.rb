39cd5d4965d0f64766467a0095d55d3ca0b21fb4
Jon Deibel <Jonathan_Deibel@intuit.com>
2015-04-13 21:49:09 -0700

    Fixed permission issues and editor error

--- lib/kitchenplan/config.rb
+++ lib/kitchenplan/config.rb
@@ -124,22 +124,17 @@ module Kitchenplan
 
     def self.create_key_with_data_bag(src)
       unless src.nil?
-
-        # Create a pem file!
-        system("sudo mkdir -p /Users/#{ENV['USERS']}/.chef")
-        system("sudo ssh-keygen -q -P=\"password\" -f /Users/#{ENV['USER']}/.chef/#{ENV['USER']}.pem")
-
         # Configure knife!
         system("sudo vendor/bin/knife configure -z")
         
         # Create a user in knife. For some reason it didn't want to accept one that i made externally
         puts("Running Command: sudo knife user create devadmin -f /Users/#{ENV['USER']}/.chef/#{ENV['USER']}.pem -a -p password -z")
-        system("sudo vendor/bin/knife user create #{ENV['USER']} -f /Users/#{ENV['USER']}/.chef/#{ENV['USER']}.pem -a -p password -e=vim -z")     
+        system("sudo vendor/bin/knife user create #{ENV['USER']} -f /Users/#{ENV['USER']}/.chef/#{ENV['USER']}.pem -e vim -a -p password -z")     
 
         # Actaully create the vault that is used :D
         puts("Running Command: knife vault create secret_vault secret_attributes '#{src.to_json}' -z")
-        system("vendor/bin/knife vault delete secret_vault secret_attributes -z")
-        system("vendor/bin/knife vault create secret_vault secret_attributes '#{src.to_json}' -z -A #{ENV['USER']}")
+        system("sudo vendor/bin/knife vault delete secret_vault secret_attributes -z")
+        system("sudo vendor/bin/knife vault create secret_vault secret_attributes '#{src.to_json}' -z -A #{ENV['USER']}")
         puts("Test")
 
         src.each do |key, array|
